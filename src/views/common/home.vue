cd925969ba523142f9e485a1b874f74db038fe5a
lijiale <18066550996@163.com>
2021-06-09 20:56:00 +0800

    前端代码提交

---
+++ src/views/common/home.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="mod-home">
+    <h3>项目介绍</h3>
+    <ul>
+      <!-- <li>renren-fast-vue基于vue、element-ui构建开发，实现<a href="https://gitee.com/renrenio/renren-fast" target="_blank">renren-fast</a>后台管理前端功能，提供一套更优的前端解决方案</li> -->
+      <li>项目分为：商城后台管理系统，商城前台用户系统两大部分</li>
+      <li>商城后台管理系统：采用前后端分离开发方式，微服务为架构搭建整套后台管理系统的前端页面和后台</li>
+      <li>商城前台用户系统：采用thymeleaf模板引擎渲染前台页面，后台使用分布式微服务架构</li>
+      <li>技术栈：springboot,mybatis-plus,redis,mysql,ElasticSearch</li>
+      <li>技术栈：springcloud Alibaba(nacos,openFeign,gateway)</li>
+      <li>技术栈：nginx,docker</li>
+      <li>技术栈：Vue element-Ui,thymeleaf</li>
+      <!-- <li>演示地址：<a href="http://demo.open.renren.io/renren-fast" target="_blank">http://demo.open.renren.io/renren-fast</a> (账号密码：admin/admin)</li> -->
+    </ul>
+    <!-- <h3>获取帮助</h3>
+    <ul>
+      <li>官方社区：<a href="https://www.renren.io/community" target="_blank">https://www.renren.io/community</a></li>
+      <li>前端Git地址：<a href="https://github.com/renrenio/renren-fast-vue" target="_blank">https://github.com/renrenio/renren-fast-vue</a></li>
+      <li>后台Git地址：<a href="https://gitee.com/renrenio/renren-fast" target="_blank">https://gitee.com/renrenio/renren-fast</a></li>
+      <li>代码生成器：<a href="https://gitee.com/renrenio/renren-generator" target="_blank">https://gitee.com/renrenio/renren-generator</a></li>
+      <li>如需关注项目最新动态，请Watch、Star项目，同时也是对项目最好的支持</li>
+    </ul> -->
+    <!-- <h3>官方QQ群</h3>
+    <ul>
+      <li>高级群：324780204(大牛云集，跟大牛学习新技能)</li>
+      <li>普通群：145799952(学习交流，互相解答各种疑问)</li>
+    </ul> -->
+    <!-- <h3>性能优化</h3>
+    <ul>
+      <li>2020.4.26 商城首页三级分类数据渲染，建立索引，压测结果吞吐量提升为原来的两倍</li>
+    </ul> -->
+    <h3>项目架构图</h3>
+    <img width="100%" src="/src/assets/img/jiagou.png">
+
+    <!-- <h3>指导老师:雷丰阳</h3>
+    <img  src="/src/assets/img/lei.png"> -->
+  </div>
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style>
+  .mod-home {
+    line-height: 1.5;
+  }
+</style>
+
